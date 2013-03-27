0fb24a208385d89bce8891075b237c20c3699df0
liuanan <liuanan123@qq.com>
2013-03-27 21:03:08 +0800

    modified:   src/action/Predicter.java

--- src/action/Predicter.java
+++ src/action/Predicter.java
@@ -23,44 +23,26 @@ public class Predicter {
 		if (model.isNormalization()) {
 			ins.normalization(model.getNormalization());
 		}
-		Stack<Node> searchSpace = new Stack<Node>();
-		List<Instance> neighbourList = initSearchSpace(ins, model, searchSpace);
-		double minDis = neighbourList.get(0).getDistance();
+		Stack<Node> searchSpace = new Stack<Node>();	//保存查询过的节点，在栈里面的节点必须有一个儿子节点没有被查询过
+		List<Instance> neighbourList = new ArrayList<Instance>();	//保存最近节点
+		double minDis = initSearchSpace(neighbourList, ins, model.getRoot(), searchSpace, 0);
 		while (searchSpace.size() != 0) {
 			Node tmp = searchSpace.pop();
 			if (tmp.getChildren() == null) {
 				continue;
 			}
 			int splitFeatureID = tmp.getSplitFeatureID();
+			
+			//检查该节点的另外一个儿子对应的区域，与以目标点为球心，以当前最短距离为半径的超球体相交
 			if (minDis >= Math.abs(tmp.getSplitFeatureValue()-neighbourList.get(0).getFeature(splitFeatureID))) {
-				//ins是tmp的左后代空间中
-				if (ins.getFeature(splitFeatureID) > tmp.getSplitFeatureValue()) {
+				//ins是tmp的左后代空间中，搜索右儿子
+				if (ins.getFeature(splitFeatureID) <= tmp.getSplitFeatureValue()) {
 					if (tmp.getChildren()[1] != null) {
-						double tmpDis = disCount(tmp.getChildren()[1].getIns(), ins);
-						if (tmpDis < minDis) {
-							tmp.getChildren()[1].setDistance(tmpDis);
-							minDis = tmpDis;
-							neighbourList.clear();
-							neighbourList.add(tmp.getChildren()[1].getIns());
-						}
-						else if (tmpDis == minDis) {
-							neighbourList.add(tmp.getChildren()[1].getIns());
-						}
-						searchSpace.push(tmp.getChildren()[1]);
+						minDis=initSearchSpace(neighbourList, ins, tmp.getChildren()[1], searchSpace, minDis);
 					}
 				}
-				else {
-					double tmpDis = disCount(tmp.getChildren()[0].getIns(), ins);
-					if (tmpDis < minDis) {
-						tmp.getChildren()[0].setDistance(tmpDis);
-						minDis = tmpDis;
-						neighbourList.clear();
-						neighbourList.add(tmp.getChildren()[0].getIns());
-					}
-					else if (tmpDis == minDis) {
-						neighbourList.add(tmp.getChildren()[0].getIns());
-					}
-					searchSpace.push(tmp.getChildren()[0]);
+				else {	//搜索左儿子
+					minDis=initSearchSpace(neighbourList, ins, tmp.getChildren()[0], searchSpace, minDis);
 				}
 			}
 		}
@@ -68,6 +50,12 @@ public class Predicter {
 		ins.setPredictLabel(neighbourList.get(0).getLabel());
 	}
 	
+	/**
+	 * 计算两个节点的距离
+	 * @param ins1	节点1保存的样本
+	 * @param ins2	节点2保存的样本
+	 * @return	两个节点的距离的平方
+	 */
 	private static double disCount(Instance ins1, Instance ins2) {
 		double dis = 0;
 		for (int i = 0; i < ins1.getFeatureSize(); ++i) {
@@ -77,10 +65,17 @@ public class Predicter {
 //		dis = Math.sqrt(dis);
 		return dis;
 	}
-	private static List<Instance> initSearchSpace(Instance ins, Model model, Stack<Node> searchSpace) { 
-		Node node = model.getRoot();
-		List<Instance> nn = new ArrayList<Instance>();
-		double minDis = 0;
+	
+	/**
+	 * 查询ins在node为根节点的树（子树）中的叶节点位置，把查询路径压入searchSpace，并且更新当前最短距离
+	 * @param nn 最近邻
+	 * @param ins	目标节点
+	 * @param node	当前搜索节点
+	 * @param searchSpace	栈
+	 * @param minDis	最小距离
+	 * @return	更新后的最小距离
+	 */
+	private static double initSearchSpace(List<Instance> nn, Instance ins, Node node, Stack<Node> searchSpace, double minDis) { 
 		while (node != null) {
 			if (nn.size() == 0) {
 				minDis = disCount(node.getIns(), ins);
@@ -111,6 +106,6 @@ public class Predicter {
 				node = node.getChildren()[1];
 			}
 		}
-		return nn;
+		return minDis;
 	}
 }
